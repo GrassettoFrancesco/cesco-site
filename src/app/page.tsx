72b2de9d1bc314e3ccca174d347829cd8841f54f
Francesco Grassetto <100849617+GrassettoFrancesco@users.noreply.github.com>
2024-11-04 22:05:33 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,8 @@ export default function Home() {
     <pre className='flex h-full flex-col items-center justify-center space-y-2.5 whitespace-pre-wrap md:space-y-5'>
       <code className='text-[2dvw] leading-[0.9] tracking-[-0.1em] lg:text-[1.4dvh] select-none'>{ascii2}</code>
       <code className='text-center text-sm md:text-base'>
-        <p>Self taught web developer</p>
-        <p>love to learn new things and I&apos;m always looking for new challenges to solve :)</p>
+        <p>Content Creator - TechCesco CEO</p>
+        <p>Carpe diem. Cogliete l'attimo, ragazzi. Rendete straordinaria la vostra vita.</p>
       </code>
     </pre>
   )
